5bf600464e18157768ac3f5cca572c82c25cbcc1
denghilbert <971431670@qq.com>
2023-09-26 13:21:16 -0400

    Modi canny

--- gradio_canny2image.py
+++ gradio_canny2image.py
@@ -64,34 +64,41 @@ def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resoluti
         results = [x_samples[i] for i in range(num_samples)]
     return [255 - detected_map] + results
 
+from PIL import Image
 
-block = gr.Blocks().queue()
-with block:
-    with gr.Row():
-        gr.Markdown("## Control Stable Diffusion with Canny Edge Maps")
-    with gr.Row():
-        with gr.Column():
-            input_image = gr.Image(source='upload', type="numpy")
-            prompt = gr.Textbox(label="Prompt")
-            run_button = gr.Button(label="Run")
-            with gr.Accordion("Advanced options", open=False):
-                num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
-                image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=64)
-                strength = gr.Slider(label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
-                guess_mode = gr.Checkbox(label='Guess Mode', value=False)
-                low_threshold = gr.Slider(label="Canny low threshold", minimum=1, maximum=255, value=100, step=1)
-                high_threshold = gr.Slider(label="Canny high threshold", minimum=1, maximum=255, value=200, step=1)
-                ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
-                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
-                seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, randomize=True)
-                eta = gr.Number(label="eta (DDIM)", value=0.0)
-                a_prompt = gr.Textbox(label="Added Prompt", value='best quality, extremely detailed')
-                n_prompt = gr.Textbox(label="Negative Prompt",
-                                      value='longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality')
-        with gr.Column():
-            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
-    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold]
-    run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
+img = process(np.uint8(Image.open('/home/yd428/ControlNet/test_imgs/IMG_0943_frame_00020.jpeg')), 'street in snow', '', '', 5, 512, 50, True, 1, 7, 971431670, 0.0, 100, 200)
+for i in range(len(img)):
+    Image.fromarray(img[i]).save('./test/test_street{}.png'.format(i))
 
+Image.open('/home/yd428/ControlNet/test_imgs/IMG_0943_frame_00020.jpeg').save('./test/test_street_gt.png')
 
-block.launch(server_name='0.0.0.0')
+#block = gr.Blocks().queue()
+#with block:
+#    with gr.Row():
+#        gr.Markdown("## Control Stable Diffusion with Canny Edge Maps")
+#    with gr.Row():
+#        with gr.Column():
+#            input_image = gr.Image(source='upload', type="numpy")
+#            prompt = gr.Textbox(label="Prompt")
+#            run_button = gr.Button(label="Run")
+#            with gr.Accordion("Advanced options", open=False):
+#                num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
+#                image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=64)
+#                strength = gr.Slider(label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
+#                guess_mode = gr.Checkbox(label='Guess Mode', value=False)
+#                low_threshold = gr.Slider(label="Canny low threshold", minimum=1, maximum=255, value=100, step=1)
+#                high_threshold = gr.Slider(label="Canny high threshold", minimum=1, maximum=255, value=200, step=1)
+#                ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
+#                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
+#                seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, randomize=True)
+#                eta = gr.Number(label="eta (DDIM)", value=0.0)
+#                a_prompt = gr.Textbox(label="Added Prompt", value='best quality, extremely detailed')
+#                n_prompt = gr.Textbox(label="Negative Prompt",
+#                                      value='longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality')
+#        with gr.Column():
+#            result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
+#    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold]
+#    run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
+#
+#
+#block.launch(server_name='0.0.0.0')
