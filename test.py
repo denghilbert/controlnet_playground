3f19c91eddf0ec89075408e52cc9b8242a3b396e
denghilbert <971431670@qq.com>
2023-09-30 13:09:03 -0400

    Add file for CLIP feature distribution test

---
+++ test.py
@@ -0,0 +1,90 @@
+from share import *
+import config
+
+import cv2
+import einops
+import gradio as gr
+import numpy as np
+import torch
+import random
+
+from pytorch_lightning import seed_everything
+from annotator.util import resize_image, HWC3
+from annotator.canny import CannyDetector
+from cldm.model import create_model, load_state_dict
+from cldm.ddim_hacked import DDIMSampler
+
+
+apply_canny = CannyDetector()
+
+model = create_model('./models/cldm_v15.yaml').cpu()
+model.load_state_dict(load_state_dict('./models/control_sd15_canny.pth', location='cuda'))
+model = model.cuda()
+ddim_sampler = DDIMSampler(model)
+
+
+def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold):
+    with torch.no_grad():
+        img = resize_image(HWC3(input_image), image_resolution)
+        H, W, C = img.shape
+
+        detected_map = apply_canny(img, low_threshold, high_threshold)
+        detected_map = HWC3(detected_map)
+
+        control = torch.from_numpy(detected_map.copy()).float().cuda() / 255.0
+        control = torch.stack([control for _ in range(num_samples)], dim=0)
+        control = einops.rearrange(control, 'b h w c -> b c h w').clone()
+
+        if seed == -1:
+            seed = random.randint(0, 65535)
+        seed_everything(seed)
+
+        if config.save_memory:
+            model.low_vram_shift(is_diffusing=False)
+
+        cond = {"c_concat": [control], "c_crossattn": [model.get_learned_conditioning([prompt + ', ' + a_prompt] * num_samples)]}
+        un_cond = {"c_concat": None if guess_mode else [control], "c_crossattn": [model.get_learned_conditioning([n_prompt] * num_samples)]}
+        shape = (4, H // 8, W // 8)
+
+        if config.save_memory:
+            model.low_vram_shift(is_diffusing=True)
+
+        model.control_scales = [strength * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else ([strength] * 13)  # Magic number. IDK why. Perhaps because 0.825**12<0.01 but 0.826**12>0.01
+
+        cond['c_crossattn'][0] = cond['c_crossattn'][0][:, :5, :]
+        import clip
+        from PIL import Image
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model_clip, preprocess = clip.load("ViT-L/14", device=device)
+        image = preprocess(Image.open("/home/youming/Desktop/controlnet_playground/test_i2i/white_dog2.png")).unsqueeze(0).to(device)
+        image_features = model_clip.encode_image(image)
+        image_features = image_features.unsqueeze(0).repeat(5, 1, 1)
+        # cond['c_crossattn'][0] = torch.cat((cond['c_crossattn'][0], image_features.float()), 1)
+        cond['c_crossattn'][0][:, 2:3, :] = image_features.float()
+        cond['c_crossattn'][0][:, 3:4, :] = image_features.float()
+        cond['c_crossattn'][0][:, 4:5, :] = image_features.float()
+        import pdb
+        pdb.set_trace()
+        samples, intermediates = ddim_sampler.sample(ddim_steps, num_samples,
+                                                     shape, cond, verbose=False, eta=eta,
+                                                     unconditional_guidance_scale=scale,
+                                                     unconditional_conditioning=un_cond)
+
+        if config.save_memory:
+            model.low_vram_shift(is_diffusing=False)
+
+        x_samples = model.decode_first_stage(samples)
+        x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c') * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
+
+        results = [x_samples[i] for i in range(num_samples)]
+    return [255 - detected_map] + results
+
+from PIL import Image
+import os
+
+img = process(np.uint8(Image.open('./test_imgs/dog.png')), 'red dog', '', '', 5, 512, 50, True, 1, 7, 971431670, 0.0, 100, 200)
+os.makedirs('./test_i2i', exist_ok=True)
+for i in range(len(img)):
+    Image.fromarray(img[i]).save('./test_i2i/combine_feature_of_white_prompt_red_{}.png'.format(i))
+
+Image.open('./test_imgs/dog.png').save('./test_i2i/dog_gt.png')
